433ad58fed90f7fd357c9b118b56b15bee0332c2
unknown <mysocialmedia69@yahoo.com>
2020-12-04 08:36:03 +0000

    update

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,98 +1,42 @@
-const { Router, request } = require("express");
-const User = require("../model/user");
-const mongoose = require("mongoose");
+const { Router} = require("express")
+const User = require("../model/user")
 
-const router = Router();
-
-
-
-router.post("/users/login", async(req, res) => {
-
-  try {
-    const user = await User.findByCredentials(req.body.email, req.body.password);
-    const token = user.generateAuthToken()
-
-    res.send({ user, token})
-
-  }catch (e) {
-
-    res.status(404).send()
-  }
-
-
-
-  
-
-})
-
+const router = Router()
 
 
 // create user
-router.post("/users", async (req, res) => {
+router.post("/users", async( req, res) => {
+   
+    const user   = new User(req.body);
 
-try {
+    try {
+      
+      await user.save()
+      res.status(201).send()
 
-  const  user = new User(req.body);
-  const token = await user.generateAuthToken();
-  res.status(201).send({ user, token})
+    }catch(e) {
+      res.status(400).send()
+    }
 
-}catch(e) {
-  res.status(500).send()
-}
-
-
-});
+})  
 
 
+// get user from database
+router.get("/users/:id", async(req, res) => {
 
-// get user from database``
-router.get("/users/:id", async (req, res) => {
   const id = req.params.id;
 
-  if (!id) return res.status(400).send();
+  try {
 
-  const isValidId = mongoose.Types.ObjectId.isValid(id);
+    const user = await User.findById(id)
 
-  if (!isValidId) return res.status(404).send();
+    if(!user)  throw new Error({ error: "user not found"})
+    res.send(user)
+  }catch(e) {
 
-  const user = await User.findById(id);
+    res.status(404).send()
+  }
+  
+})
 
-  if (!user) return res.status(404).send();
-  res.send(user);
-});
-
-
-
-// update user
-router.patch("/users/:id", async (req, res) => {
-  const id = req.params.id;
-
-  if (!id) return res.status(400).send({ error: "please provide an email" });
-
-  const validfields = ["name", "email", "password"];
-  const fields = Object.keys(req.body);
-
-  const isValid = fields.every((field) => validfields.includes(field));
-
-  if (!isValid) return res.status(400).send({ error: "invalid field" });
-
-  const user = await User.findByIdAndUpdate(id, req.body, { new: true });
-
-  if (!user) return res.status(404).send({ error: "user not found!" });
-  res.send(user);
-});
-
-
-
-// delete user
-router.delete("/users/:id", async (req, res) => {
-  const id = req.params.id;
-
-  if (!id) return res.status(400).send();
-
-  const user = await User.findByIdAndDelete(id);
-
-  if (!user) return res.status(404).send(1);
-  res.send();
-});
 module.exports = router;
--- tests/user.test.js
+++ tests/user.test.js
@@ -1,47 +1,65 @@
+const mongoose = require("mongoose")
 const app = require("../server/app")
-const request = require("supertest")
-const  mongoose = require("mongoose")
 const User = require("../server/model/user")
+const request = require("supertest")
 
+
+const userOneId = mongoose.Types.ObjectId()
+const userTwoId = mongoose.Types.ObjectId()
 
 const userOne = {
-  name: "kofi  arhin",
+  _id: userOneId,
+  name: "kofi arhin",
   email: "kofiarhin@gmail.com",
-  password: "password123",
+  password: "password"
 }
-
-
-beforeEach( async() => {
-    await User.deleteMany()
-    await new User(userOne).save()
-})
-
-// user two
 const userTwo = {
+  _id: userTwoId,
   name: "lebron james",
   email: "lebron@gmail.com",
   password: "password123"
 }
 
-afterAll(async() => {
+// setup teardown
+beforeEach ( async() => {
 
+  await User.deleteMany()
+  await new User(userOne).save()
+})
+
+afterAll(async() => {
   await mongoose.connection.close()
 })
 
 
+// passing test
+test("should just pass", () => {})
 
+// test create user
+test("create user", async() => {
 
-
-test("login user with valid credentials", async() => {
-
-  const response = await request(app).post('/users/login').send({ email: userOne.email, password: userOne.password}).expect(200)
-
-expect(response.body.user).toHaveProperty("name", userOne.name)
-
+  const response = await request(app).post("/users").send(userTwo).expect(201)
 })
 
 
-test("cannot login user with invalid credentials", async() => {
+// should not create duplicate users
+test("should not create duplicate user", async() => {
+    const response = await request(app).post("/users").send(userOne).expect(400)
 
-  const response = await request(app).post("/users/login").send({ email: userOne.email, password: "password"})
+});
+
+
+// should not create user with invalid fields
+test("should not create user with invalid fields", async() => {
+
+     const response = await request(app).post("/users").send().expect(400)
+})
+
+
+// get user from database
+test("get user from database", async() => {
+
+  const response = await request(app).get("/users/"+userOne._id).send().expect(200);
+
+  console.log(response.body)
 })
